c51471eed675c5e1251a0341bea4eefc744f5c09
NICO GARCIA <07nico.garcia@gmail.com>
2025-04-07 15:45:40 +0200

    ok

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,14 @@
-import "../styles/NavBar.css"
+import "../styles/NavBar.css";
 
 export default function NavBar() {
-    return (
-        <nav>
-            <img src="" alt="logo" />
-            <ul>
-                <li>A propros</li>
-                <li>personnages</li>
-                <li>acceuil</li>
-            </ul>
-
-        </nav>
-    )
+	return (
+		<nav>
+			<img src="" alt="logo" />
+			<ul>
+				<li>A propros</li>
+				<li>personnages</li>
+				<li>acceuil</li>
+			</ul>
+		</nav>
+	);
 }
